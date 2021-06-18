12f531866ee91c1deb66306dfcd5d2798c482621
dodja12 <78220879+dodja12@users.noreply.github.com>
2021-06-18 19:54:06 -0300

    m1

---
+++ music/music/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+namespace music
+{
+    class ms
+    {
+        static void Main()
+        {
+            
+            Console.WriteLine("write music:");
+            List<string> ms = new List<string>();
+            string m;
+            int c = 0;
+            while (true)
+            {
+                c += 1;
+                m = Console.ReadLine();
+                ms.Add(m);
+                if (c == 16)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("music:");
+            foreach (string tx in ms)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("========================================");
+                Console.WriteLine("♫ " + tx + " ♫");
+            }
+            Console.ReadLine();
+        }
+    }
+}
